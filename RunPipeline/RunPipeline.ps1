d7c5e5f3207f6bae3edfb9b5b6eddb6c185b21b7
Brian Werth <brian.werth@Cavallo.com>
2023-06-20 08:43:51 -0400

    chore: fixing spacing [v2.1_disablewrite]

--- RunPipeline/RunPipeline.ps1
+++ RunPipeline/RunPipeline.ps1
@@ -427,7 +427,7 @@ try {
         -buildArtifactFolder $buildArtifactFolder `
         -CreateRuntimePackages:$CreateRuntimePackages `
         -appBuild $appBuild -appRevision $appRevision `
-        -uninstallRemovedApps`
+        -uninstallRemovedApps `
         -NewBcContainer { Param([Hashtable]$parameters) New-BcContainer @parameters; Invoke-ScriptInBcContainer $parameters.ContainerName -scriptblock { $progressPreference = 'SilentlyContinue' }; Set-BcContainerServerConfiguration $parameters.ContainerName DisableWriteInsideTryFunctions false;  Restart-BCContainer $parameters.ContainerName} 
 
     if ($containerBaseFolder) {
