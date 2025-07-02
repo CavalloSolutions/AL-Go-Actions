0772ef39ca50a98f2b3926c7591ee11cca05d497
Luke Dobben <luke.dobben@Cavallo.com>
2025-07-02 14:48:28 -0400

    chore: add backtick

--- RunPipeline/RunPipeline.ps1
+++ RunPipeline/RunPipeline.ps1
@@ -524,7 +524,7 @@ try {
         -pageScriptingTestResultsFolder (Join-Path $buildArtifactFolder 'PageScriptingTestResultDetails') `
         -CreateRuntimePackages:$CreateRuntimePackages `
         -appBuild $appBuild -appRevision $appRevision `
-        -uninstallRemovedApps
+        -uninstallRemovedApps `
         -NewBcContainer { Param([Hashtable]$parameters) New-BcContainer @parameters; Invoke-ScriptInBcContainer $parameters.ContainerName -scriptblock { $progressPreference = 'SilentlyContinue' }; Set-BcContainerServerConfiguration $parameters.ContainerName DisableWriteInsideTryFunctions false;  Restart-BCContainer $parameters.ContainerName} 
 
     if ($containerBaseFolder) {
