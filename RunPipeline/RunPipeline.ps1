0b523dd2bf144001d373f7297ab553f58db798ca
Daniel Smith <daniel.smith@cavallo.com>
2026-04-03 10:28:35 -0400

    fix: apply new bc container change on v8.3

--- RunPipeline/RunPipeline.ps1
+++ RunPipeline/RunPipeline.ps1
@@ -563,7 +563,8 @@ try {
         -pageScriptingTestResultsFolder (Join-Path $buildArtifactFolder 'PageScriptingTestResultDetails') `
         -CreateRuntimePackages:$CreateRuntimePackages `
         -appBuild $appBuild -appRevision $appRevision `
-        -uninstallRemovedApps
+        -uninstallRemovedApps `
+        -NewBcContainer { Param([Hashtable]$parameters) New-BcContainer @parameters; Invoke-ScriptInBcContainer $parameters.ContainerName -scriptblock { $progressPreference = 'SilentlyContinue' }; Set-BcContainerServerConfiguration $parameters.ContainerName DisableWriteInsideTryFunctions false; Restart-BCContainer $parameters.ContainerName}
 
     if ($containerBaseFolder) {
         Write-Host "Copy artifacts and build output back from build container"
